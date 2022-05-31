5c7c7861cbf076956bba2ecb3b38f097fe4355ee
Nola <juanmanolasco0105@gmail.com>
2022-05-31 19:56:11 +0200

    Create itinerary entity

---
+++ src/entities/itinerary.js
@@ -0,0 +1,12 @@
+class Itinerary {
+    constructor(name, department, teacher, endDate, books, students) {
+        this.name = name;
+        this.department = department;
+        this.teacher = teacher;
+        this.endDate = endDate;
+        this.books = books;
+        this.students = students;
+    }
+}
+
+module.exports = Itinerary;
