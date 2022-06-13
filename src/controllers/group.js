7a39a311037a718ff4f9f8a30511deb2d410d84d
Nola <juanmanolasco0105@gmail.com>
2022-06-13 16:47:37 +0200

    Change get user groups by user id

--- src/controllers/group.js
+++ src/controllers/group.js
@@ -20,8 +20,31 @@ const getUserGroups = (req, res) => {
 
     db.select('*').from('users_groups').where('id_user', id)
     .then(
-        (groups) => {
-            return res.status(200).send(groups)
+        async (userGroups) => {
+            console.log(userGroups);
+            if (userGroups.length !== 0) {
+                const groupList = []
+                let promises = []
+                promises = userGroups.map(async (group) => {
+                    await db('groups').where('id_group', group.id_group)
+                    .then(
+                        groups => {
+                            const group = groups[0]
+                            groupList.push(group)
+                        }
+                    )
+                    .catch((err) => {
+                        return res.status(500).json({
+                            ok: false,
+                            msg: "Error al encontrar grupos",
+                        })
+                    })
+                })
+                await Promise.all(promises)
+                return res.status(200).send(groupList)
+            } else {
+                return res.status(200).send(userGroups)
+            }
         }
     )
     .catch((err) => {
