c7c9d1724194ba544e3593d370d1862c83ee7591
Nola <juanmanolasco0105@gmail.com>
2022-06-17 11:25:55 +0200

    Code cleaning

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,28 +1,15 @@
 const {response} = require('express');
-const db = require('../database/db-config');
-const User = require('../models/user');
 
 const {generateJWT} = require('../helper/jwt')
-
-
-
 
 const {createUser, loginUser} = require('./user')
 
 const register = async (req, res = response) => {
-    // const {name, password, email, role, pin} = req.body;
-
-    // const user = new User(name, password, email, role, pin);
-
     createUser(req, res);
     
 }
 
 const login = async (req, res = response) => {
-
-    // const {name, password, email, role, pin} = req.body;
-
-    // const user = new User(name, password, email, role, pin);
 
     loginUser(req, res);
     
